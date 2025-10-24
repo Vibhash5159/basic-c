73a889c8cff5d2c6ff83ff5e80f744a95f7ad408
CODE_VIBHASH <tripathivibhash371@gmail.com>
2025-10-25 01:17:12 +0530

    to find the size of data type

---
+++ size.c
@@ -0,0 +1,15 @@
+# include<stdio.h>
+int main(){
+   int a,b,c,d,e;
+   a= sizeof (int);
+   b= sizeof (double);
+   c= sizeof(char);
+   d= sizeof (float);
+   e= sizeof (long);
+printf("%d \n",a);
+printf("%d \n",b);
+printf("%d \n",c);
+printf("%d \n",d);
+printf("%d \n",e);
+return 0;
+}
