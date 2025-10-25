2722bf01aa54a49dbcf0e63c7ecc50314a3fd4ec
CODE_VIBHASH <tripathivibhash371@gmail.com>
2025-10-25 19:54:53 +0530

    guess a secret number
    It is a mini game in which we have to guess a secret number . This code is made by using basics of c language

---
+++ guess.c
@@ -0,0 +1,19 @@
+# include<stdio.h>
+
+int main(){
+    int SecretNum=5;
+    int guess;
+    while(guess!=5){
+          
+        printf("Enter a number= ");
+          scanf("%d",&guess);
+
+        printf("Better Luck Next Time!\n");
+        printf("Wanna try again !!!\n");
+         
+    
+        }
+   printf("You Win!!");
+
+return 0;
+}
